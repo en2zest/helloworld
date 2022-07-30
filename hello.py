15be33e355d106e508c90db7533f763cf7c4379b
MJR <en2zest@#hotmail.com>
2022-07-30 09:44:45 -0400

    modified hello messages

---
+++ hello.py
@@ -0,0 +1,9 @@
+# first python progrram
+def hello(msn);
+    # works hello: str
+    print(f"hello {msg}!")
+
+    if _name_ == "_main_":
+        hello("world")
+        hello("Alice")
+        hello("Bob")
